6f85bae1ff162311630da44e0764ec753e2d1e30
Henry Ladstätter <henry.ladstaetter08@gmail.com>
2024-12-06 16:29:49 +0100

    Refactor arch-setup.sh for improved formatting and validation in user prompts

--- arch-setup.sh
+++ arch-setup.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 # This script is for setting up Arch Linux
-echo "Which keylayout do you want (A to print all options)": 
+echo "Which keylayout do you want (A to print all options)":
 read keylayout
-if [ $keylayout == "A" ]
-then
+if [ $keylayout == "A" ]; then
     localectl list-keymaps
     echo "Which keylayout do you want: "
     read keylayout
@@ -18,27 +17,34 @@ if [ "$(pwd)" != "/root" ]; then
     cd || exit
     echo "Changed directory to /root"
 fi
-printf "Do you want to update the time : Y/N"
+printf "Do you want to update the time Y/N : "
 read time
-if [ $time = "Y" ]
-then
+time=$(echo "$time" | tr '[:upper:]' '[:lower:]')
+if [ $time = "y" ]; then
     timedatectl set-ntp true
     if [ $? -eq 0 ]; then
         echo "Time updated successfully."
     else
         echo "Failed to update time."
     fi
-fi 
+fi
 
 echo "Starting partionioning of the disk"
 echo "Warning: This will delete all data on the disk"
 printf "Which disk do you want to partition (A to print all options): "
 read disk
-if [ $disk == "A" ]
-then
-    lsblk
-    printf "Which disk do you want to partition: "
-    read disk
+if [ $disk == "A" ]; then
+    lsblk | grep -E "disk|part" | grep -vE "loop|zram"
+fi
+printf "Which disk do you want to partition: "
+read disk
+if [ -z "$disk" ]; then
+    echo "Disk not provided"
+    exit 1
+fi
+if ! lsblk | grep -E "$disk"; then
+    echo "Disk not found"
+    exit 1
 fi
 echo "Info Starting partitioning of $disk"
 printf "How much GB for Linux Swap(Min: 4G) : "
@@ -47,4 +53,4 @@ printf "How much GB for Linux Boot(Min :  1GB) : "
 read boot
 printf "How much GB for Linux Root(Min: 20GB) : "
 read root
-sudo fdisk /dev/$disk 
+sudo fdisk /dev/$disk
