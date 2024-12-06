5c743552075b4b93e39eed07a91287a25ba6a4e7
Henry Ladstätter <henry.ladstaetter08@gmail.com>
2024-12-06 17:32:09 +0100

    Improve keylayout handling and enhance disk partitioning prompts in arch-setup.sh

--- arch-setup.sh
+++ arch-setup.sh
@@ -2,16 +2,25 @@
 # This script is for setting up Arch Linux
 echo "Which keylayout do you want (A to print all options)":
 read keylayout
-if [ $keylayout == "A" ]; then
+if [ "$keylayout" = "A" ] || [ "$keylayout" = "a" ]; then
     localectl list-keymaps
-    echo "Which keylayout do you want: "
-    read keylayout
 fi
-loadkeys "$keylayout"
+echo "Which keylayout do you want: "
+read keylayout
+if [ -z "$keylayout" ]; then
+    echo "Keylayout not provided ! Continuing with the currently selectet keylayout."
+    current_keylayout=$(localectl status | grep "VC Keymap" | awk '{print $3}')
+    echo "Current keylayout is: $current_keylayout"
+fi
+loadkeys "$keylayout" | grep -q "loadkeys : Unable to open"
+if [ $? -eq 1 ]; then
+    echo "Failed to load keylayout ! Continuing with the currently selectet keylayout."
+    current_keylayout=$(localectl status | grep "VC Keymap" | awk '{print $3}')
+    echo "Current keylayout is: $current_keylayout"
+    exit 1
+fi
 if [ $? -eq 0 ]; then
     echo "Keylayout loaded successfully."
-else
-    echo "Failed to load keylayout."
 fi
 if [ "$(pwd)" != "/root" ]; then
     cd || exit
@@ -30,15 +39,14 @@ if [ $time = "y" ]; then
 fi
 
 echo "Starting partionioning of the disk"
-echo "Warning: This will delete all data on the disk"
-printf "Which disk do you want to partition (A to print all options): "
-read disk
-if [ $disk == "A" ]; then
-    lsblk | grep -E "disk|part" | grep -vE "loop|zram"
-fi
+echo -e "\e[31mWarning: This will delete all data on the disk\e[0m"
+
 while true; do
-    printf "Which disk do you want to partition: "
-    read disk
+    printf "Which disk do you want to partition (A to print all options): "
+    read -r disk
+    if [ "$disk" = "A" ] || [ "$disk" = "a" ]; then
+    lsblk | grep -E "disk|part" | grep -vE "loop|zram"
+    fi
     if [ -z "$disk" ]; then
         echo "Disk not provided"
     elif lsblk | grep -q "$disk"; then
@@ -49,7 +57,6 @@ while true; do
 done
 echo "Info Starting partitioning of $disk"
 disk_size=$(lsblk -bno SIZE /dev/$disk | grep -m 1 -E "^.*$")
-
 
 while true; do
     printf "How much GB for Linux Swap (Min: 1G): "
@@ -85,24 +92,24 @@ while true; do
     break
 done
 {
-  echo "g"                        # Create a new DOS partition table
-  echo "n"                        # Add a new partition (Boot)
-  echo "p"                        # Primary partition
-  echo "1"                        # Partition number 1
-  echo                           # Default - start at beginning of disk
-  echo "+$boot"              # Boot partition size
-  echo "n"                        # Add a new partition (Swap)
-  echo "p"                        # Primary partition
-  echo "2"                        # Partition number 2
-  echo                           # Default - start immediately after boot
-  echo "+$swap"              # Swap partition size
-  echo "n"                        # Add a new partition (Root)
-  echo "p"                        # Primary partition
-  echo "3"                        # Partition number 3
-  echo                           # Default - start immediately after swap
-  echo "$root"               # Root partition size
-  echo "t"                        # Change partition type
-  echo "2"                        # Select partition 2 (swap)
-  echo "82"                       # Set type to Linux swap
-  echo "w"                        # Write the changes and exit
+    echo "g"       # Create a new DOS partition table
+    echo "n"       # Add a new partition (Boot)
+    echo "1"       # Partition number 1
+    echo           # Default - start at beginning of disk
+    echo "+$boot"G # Boot partition size
+    echo "t"       # Change partition type
+    echo "1"       # Select partition 1 (boot)
+    echo "1"       # Set type to EFI System
+    echo "n"       # Add a new partition (Swap)
+    echo "2"       # Partition number 2
+    echo           # Default - start immediately after boot
+    echo "+$swap"G # Swap partition size
+    echo "n"       # Add a new partition (Root)
+    echo "3"       # Partition number 3
+    echo           # Default - start immediately after swap
+    echo "$root"G  # Root partition size
+    echo "t"       # Change partition type
+    echo "2"       # Select partition 2 (swap)
+    echo "82"      # Set type to Linux swap
+    echo "w"       # Write the changes and exit
 } | fdisk "/dev/$disk"
