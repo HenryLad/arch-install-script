34cff4c55b286be352713c25ef38e136086f2267
Henry Ladstätter <henry.ladstaetter08@gmail.com>
2024-12-10 22:26:00 +0100

    Add user and root password setup, GRUB installation, and various package installations in arch-setup.sh

--- arch-setup.sh
+++ arch-setup.sh
@@ -296,3 +296,110 @@ else
     echo -e "$ERROR Failed to enable NetworkManager."
 fi
 
+# Setting up the root password
+echo "Setting up the root password"
+printf "Enter the root password: "
+read -s root_password
+passwd root <<< "$root_password" >> /dev/null
+if [ $? -eq 0 ]; then
+    echo -e "$SUCESS Root password set successfully."
+else
+    echo -e "$ERROR Failed to set root password."
+fi
+
+# Setting up the user
+echo "Setting up the user"
+printf "Enter the username: "
+read username
+useradd -m -g users -G wheel -s /bin/bash "$username"
+if [ $? -eq 0 ]; then
+    echo -e "$SUCESS User '$username' created successfully."
+else
+    echo -e "$ERROR Failed to create user '$username'."
+fi
+printf "Enter the password for user '$username': "
+read -s user_password
+passwd "$username" <<< "$user_password" >> /dev/null
+if [ $? -eq 0 ]; then
+    echo -e "$SUCESS Password set for user '$username' successfully."
+else
+    echo -e "$ERROR Failed to set password for user '$username'"
+fi
+
+# Setting up GRUB Bootloader for EFI Systems 
+pacman -S grub efibootmgr --noconfirm >> /dev/null
+if [ $? -eq 0 ]; then
+    echo -e "$SUCESS GRUB Bootloader installed successfully."
+else
+    echo -e "$ERROR Failed to install GRUB Bootloader."
+fi
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB >> /dev/null
+if [ $? -eq 0 ]; then
+    echo -e "$SUCESS GRUB Bootloader installed successfully on the System."
+else
+    echo -e "$ERROR Failed to install GRUB Bootloader."
+fi
+grub-mkconfig -o /boot/grub/grub.cfg >> /dev/null
+if [ $? -eq 0 ]; then
+    echo -e "$SUCESS GRUB Bootloader configured successfully."
+else
+    echo -e "$ERROR Failed to configure GRUB Bootloader."
+fi
+
+sudo pacman -S xorg [xf86-video-your gpu type] --noconfirm
+if [ $? -eq 0 ]; then
+    echo -e "$SUCESS Xorg installed successfully."
+else
+    echo -e "$ERROR Failed to install Xorg."
+fi
+
+sudo pacman -S sddm
+sudo systemctl enable sddm
+if [ $? -eq 0 ]; then
+    echo -e "$SUCESS SDDM installed successfully."
+else
+    echo -e "$ERROR Failed to install SDDM."
+fi
+#Installing KDE Plasma
+sudo pacman -S plasma alacritty dolphin ark kwrite kcalc spectacle krunner partitionmanager packagekit-qt5
+if [ $? -eq 0 ]; then
+    echo -e "$SUCESS KDE Plasma installed successfully."
+else
+    echo -e "$ERROR Failed to install KDE Plasma."
+fi
+# Installing Audio and Bluetooth
+sudo pacman -S alsa-utils bluez bluez-utils
+if [ $? -eq 0 ]; then
+    echo -e "$SUCESS Audio and Bluetooth installed successfully."
+else
+    echo -e "$ERROR Failed to install Audio and Bluetooth."
+fi
+sudo systemctl enable bluetooth.service
+# Installing Optional Programms
+sudo pacman -S firefox vlc libreoffice openssh wget git fastfetch docker docker-compose 
+if [ $? -eq 0 ]; then
+    echo -e "$SUCESS Optional Programms installed successfully."
+else
+    echo -e "$ERROR Failed to install Optional Programms."
+fi
+# Enabling OpenSSH daemon and CUPS printer service
+sudo systemctl enable sshd.service
+sudo systemctl enable --now cups.service
+if [ $? -eq 0 ]; then
+    echo -e "$SUCESS OpenSSH daemon and CUPS printer service enabled successfully."
+else
+    echo -e "$ERROR Failed to enable OpenSSH daemon and CUPS printer service."
+fi
+# Enabling Docker
+sudo systemctl enable docker.service
+if [ $? -eq 0 ]; then
+    echo -e "$SUCESS Docker enabled successfully."
+else
+    echo -e "$ERROR Failed to enable Docker."
+fi
+# Installing YAY 
+git clone https://aur.archlinux.org/yay.git
+cd yay || exit 
+makepkg -si
+cd .
+rm -rf yay 
