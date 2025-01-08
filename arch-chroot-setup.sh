0d293b996d40ebeb0d7c0f56629f3e64eb2d5cb7
Henry Ladstätter <henry.ladstaetter08@gmail.com>
2025-01-08 11:42:22 +0100

    Enhance arch-chroot-setup.sh to improve keylayout setup, system upgrade, and time configuration; add GPU driver installation logic and streamline user prompts for better usability

--- arch-chroot-setup.sh
+++ arch-chroot-setup.sh
@@ -14,10 +14,37 @@ display_message() {
         echo -e "$ERROR $3"
     fi
 }
+# Setting up the keylayout
+echo "Setting up the keylayout"
+printf "Enter your keylayout (e.g. de-latin1): "
+read -r keylayout
+loadkeys "$keylayout"
+display_message $? "Keylayout set successfully." "Failed to set keylayout."
+# Starting System upgrade and update
+echo -e "$WARNING Starting full System upgrade and update."
+pacman -Sy --noconfirm >> /dev/null
+display_message $? "System updated successfully." "Failed to update system."
+pacamn -Syu --noconfirm >> /dev/null
+display_message $? "System upgraded successfully." "Failed to update system."
+
+# Setting up time 
+echo "Setting up the time"
+timedatectl set-ntp true
+display_message $? "Time updated successfully." "Failed to update time."
+# Updating the mirrorlist
+echo "Updating the mirrorlist"
+pacman -S reflector --noconfirm >>/dev/null
+display_message $? "Reflector installed successfully." "Failed to install reflector."
+reflector --latest 200 --sort rate --save /etc/pacman.d/mirrorlist
+display_message $? "Mirrorlist updated successfully." "Failed to update mirrorlist."
+# Updating Keyring
+echo "Updating Pacman Keyring"
+pacman -S archlinux-keyring --noconfirm >>/dev/null
+display_message $? "Pacman Keyring updated successfully." "Failed to update Pacman Keyring."
 # Setting up Language
 echo "Setting up the language"
 printf "Enter your language (e.g. en_US.UTF-8, de_DE.UTF-8): "
-read language
+read -r language
 echo "LANG=$language" >/etc/locale.conf
 echo "$language UTF-8" >>/etc/locale.gen
 locale-gen
@@ -25,11 +52,11 @@ display_message $? "Language set successfully." "Failed to set language."
 
 # Setting the hostname
 printf "Set your hostname: "
-read hostname
+read -r hostname
 echo "$hostname" >/etc/hostname
 display_message $? "Hostname set successfully." "Failed to set hostname."
 # Setting up the hosts file
-echo "127.0.0.1   localhost" >>/etc/hosts
+echo "127.0.0.1   localhost" >/etc/hosts
 echo "::1         localhost" >>/etc/hosts
 echo "127.0.1.1   $hostname.localdomain $hostname" >>/etc/hosts
 display_message $? "Hosts file set successfully." "Failed to set hosts file."
@@ -46,17 +73,17 @@ display_message $? "Password set for root successfully." "Failed to set password
 # Setting up the user
 echo "Setting up the user"
 printf "Enter the username: "
-read username
+read -r username
 useradd -m -g users -G wheel -s /bin/bash "$username"
 if [ $? -eq 0 ]; then
-    echo -e "$SUCESS User '$username' created successfully."
+    echo -e "$SUCCESS User '$username' created successfully."
 else
     echo -e "$ERROR Failed to create user '$username'."
 fi
 echo "Setting up the user password"
 passwd "$username"
 if [ $? -eq 0 ]; then
-    echo -e "$SUCESS Password set for user '$username' successfully."
+    echo -e "$SUCCESS Password set for user '$username' successfully."
 else
     echo -e "$ERROR Failed to set password for user '$username'"
 fi
@@ -68,8 +95,24 @@ grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB >>/d
 display_message $? "GRUB Bootloader installed successfully." "Failed to install GRUB Bootloader."
 grub-mkconfig -o /boot/grub/grub.cfg >>/dev/null
 display_message $? "GRUB Bootloader configured successfully." "Failed to configure GRUB Bootloader."
+# Getting Grapic Card Informations 
+intel=$(lspci -v -nn -d ::03xx | grep Intel > /dev/null)
+amd=$(lspci -v -nn -d ::03xx | grep AMD > /dev/null)
+nvidia=$(lspci -v -nn -d ::03xx | grep NVIDIA > /dev/null)
 
-sudo pacman -S xorg [xf86-video-your gpu type] --noconfirm
+if [ -n "$intel" ]; then
+    sudo pacman -S xf86-video-intel --noconfirm
+    display_message $? "Intel video driver installed successfully." "Failed to install Intel video driver."
+elif [ -n "$amd" ]; then
+    sudo pacman -S xf86-video-amdgpu --noconfirm
+    display_message $? "AMD video driver installed successfully." "Failed to install AMD video driver."
+elif [ -n "$nvidia" ]; then
+    sudo pacman -S nvidia nvidia-utils --noconfirm
+    display_message $? "NVIDIA video driver installed successfully." "Failed to install NVIDIA video driver."
+else
+    echo -e "$WARNING No supported GPU found. Skipping GPU driver installation."
+fi
+sudo pacman -S xorg  --noconfirm
 display_message $? "Xorg installed successfully." "Failed to install Xorg."
 sudo pacman -S sddm --noconfirm
 sudo systemctl enable sddm
